51510c3ae815a074ae96f6e770de07663c044988
宮崎貴史 <>
2020-03-21 08:57:38 +0900

    ボタン追加 onChangeの処理の拡張

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { Field, Form, Formik, useField } from 'formik'
+import { Form, Formik, useField } from 'formik'
 import { TextField, Button } from '@material-ui/core'
 
-const MyField = ({ disabled, ...props }) => {
-  const [field, meta] = useField(props)
+// このコンポーネントでonChange時の動作を実行時に変更したい
+// convertTargetValue で e.target.value の値を変更する
+// changeCb でonChange後の後処理を追加(いらないかもしれない。やるとしたら他でやる)
+const MyTextField = ({
+  disabled,
+  convertTargetValue = undefined,
+  changeCb = undefined,
+  ...props
+}) => {
+  const [field, meta, helper] = useField(props)
   const errorText = meta.error && meta.touched ? meta.error + '' : ''
 
   return (
@@ -13,9 +21,42 @@ const MyField = ({ disabled, ...props }) => {
         error={!!errorText}
         helperText={errorText}
         disabled={disabled}
+        onChange={e => {
+          if (convertTargetValue !== undefined) {
+            helper.setValue(convertTargetValue(e.target.value))
+          } else {
+            helper.setValue(e.target.value)
+          }
+
+          if (changeCb) changeCb(e.target.value)
+        }}
       />
-      <div>{errorText}</div>
     </div>
+  )
+}
+
+// Formikのfield,metaと、disabled, onCustomChangeを受け取る
+const TxtField = ({ field, meta, disabled, onCustomChange }) => {
+  const errorText = meta.error && meta.touched ? meta.error + '' : ''
+
+  return (
+    <div>
+      <TextField
+        {...field}
+        error={!!errorText}
+        helperText={errorText}
+        disabled={disabled}
+        onChange={onCustomChange || field.onChange}
+      />
+    </div>
+  )
+}
+
+const AddButton = ({ increment, ...props }) => {
+  const [helpers] = useField(props)
+
+  return (
+    <Button onClick={() => helpers.setValue(increment)}>ADD {increment}</Button>
   )
 }
 
@@ -35,14 +76,24 @@ const App = () => {
   return (
     <div>
       <Formik
-        initialValues={{ firstName: '', lastName: '' }}
+        initialValues={{ firstName: '', lastName: '', count: 0 }}
         validate={validate}
         onSubmit={values => alert(JSON.stringify(values, null, 2))}
       >
         {({ values }) => (
           <Form>
-            <Field name={'firstName'} />
-            <MyField name={'lastName'} disabled={values.firstName === ''} />
+            <MyTextField
+              name={'firstName'}
+              changeCb={result => console.log(result)}
+            />
+            <MyTextField
+              name={'lastName'}
+              disabled={values.firstName === ''}
+              convertTargetValue={str => (str === 'a' ? '100' : str)}
+            />
+            <AddButton name="count" increment={5} />
+            <AddButton name="count" increment={10} />
+            <AddButton name="count" increment={15} />
             <Button type="submit">Submit</Button>
             <pre>{JSON.stringify(values, null, 2)}</pre>
           </Form>
